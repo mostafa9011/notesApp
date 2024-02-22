01b7c98df204d929c165d61dc4ad3e9eb6f0f219
mostafa <mostafarasekh5@gamil.com>
2024-02-22 13:01:56 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        NotesView.id: (context) => const NotesView(),
+      },
+      initialRoute: NotesView.id,
+    );
+  }
+}
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+  static const String id='notesView';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
