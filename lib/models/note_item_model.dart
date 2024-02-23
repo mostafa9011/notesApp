daa8a001284709a62c1f3ae1a8f4e450cf85c385
mostafa <mostafarasekh5@gamil.com>
2024-02-22 20:47:38 -0800

    create note item model , and add hive to prcoject

---
+++ lib/models/note_item_model.dart
@@ -0,0 +1,10 @@
+class NoteItemModel {
+  final String title, subtitle, date;
+  final int color;
+  const NoteItemModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color,
+  });
+}
